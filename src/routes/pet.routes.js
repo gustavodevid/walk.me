73790387ffeec0444090d7365f21ac948517fc45
gusta <gusta@email.com>
2024-08-19 18:32:11 -0300

    Resolve erro do ESM

--- src/routes/pet.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-router.get('/', () => {
-    console.log("pets");
-});
-export default router;
--- src/routes/routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import petRoutes from './pet.routes';
-const router = Router();
-router.use('/v1/pets', petRoutes);
-export default router;
